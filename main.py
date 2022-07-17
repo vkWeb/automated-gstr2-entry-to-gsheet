e8eb67e1b396ba2d0d2c84fb2ee4adb9335e0fad
Vivek Agrawal <vivekmittalagrawal@gmail.com>
2022-07-17 17:03:42 +0530

    Remove 2a support and robustify 2b

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ def write_to_sheets(
     spreadsheet_id,
     supplier_name="",
 ):
-
     with build("sheets", "v4", credentials=CREDENTIALS) as service:
         values = [
             [
@@ -41,9 +40,11 @@ def write_to_sheets(
         n = 0
         total_wait = 0
         max_backoff = 32
-        deadline = 300
+        deadline = 600
 
-        while total_wait < deadline:
+        result = None
+
+        while total_wait < deadline and result is None:
             try:
                 result = (
                     service.spreadsheets()
@@ -56,8 +57,6 @@ def write_to_sheets(
                     )
                     .execute()
                 )
-                print(f"Success! {result.get('updatedCells')} cells updated.")
-                break
             except HttpError:
                 wait_time = min(math.pow(2, n), max_backoff)
                 sleep(wait_time)
@@ -65,43 +64,27 @@ def write_to_sheets(
                 n = n + 1
 
 
-def gstr2acsv_to_google_sheets(gstr2a_file, sheet_name, spreadsheet_id, row_num):
-    with open(
-        gstr2a_file,
-        mode="r",
-        encoding="utf-8",
-        newline="",
-    ) as csvfile:
-
-        csvreader = csv.DictReader(csvfile)
-
-        for row in csvreader:
-            if row["Integrated Tax"] == "0":
-                gst_type = "INTRA"
-            else:
-                gst_type = "INTER"
-
-            write_to_sheets(
-                supplier_gstin=row["GSTIN of supplier"],
-                gst_type=gst_type,
-                gst_rate=row["Rate (%)"],
-                taxable_value=row["Taxable Value"],
-                range_name=f"{sheet_name}!A{row_num}:D{row_num}",
-                spreadsheet_id=spreadsheet_id,
-            )
-            row_num = row_num + 1
-
-
 def gstr2bjson_to_google_sheets(gstr2b_file, sheet_name, spreadsheet_id, row_num):
+
+    print("The magic is under progress...")
+
     with open(gstr2b_file, "r") as f:
         json_data = json.load(f)
+
+    total_entry_count = 0
+    total_write_count = 0
 
     for b2b_supplier in json_data["data"]["docdata"]["b2b"]:
         supplier_gstin = b2b_supplier["ctin"]
         supplier_name = b2b_supplier["trdnm"]
         for inv in b2b_supplier["inv"]:
             for inv_item in inv["items"]:
+
+                total_entry_count = total_entry_count + 1
+
                 if inv_item["rt"] == 0:
+                    print("Skipping entry of {}. Invoice: {}. Reason: rate is 0%.".format(
+                        supplier_name, inv["inum"]))
                     continue
 
                 if inv_item.get("igst") is not None and inv_item.get("igst") > 0:
@@ -118,19 +101,22 @@ def gstr2bjson_to_google_sheets(gstr2b_file, sheet_name, spreadsheet_id, row_num
                     range_name=f"{sheet_name}!A{row_num}:D{row_num}",
                     spreadsheet_id=spreadsheet_id,
                 )
+
                 row_num = row_num + 1
+                total_write_count = total_write_count + 1
+
+    print("Total entries = {}".format(total_entry_count))
+    print("Total entries written = {}".format(total_write_count))
+    print("Magic is done!")
 
 
 if __name__ == "__main__":
-    gstr2a_file = "GSTR2A-INVOICE-22AEZPA0778G1ZF-102021-102021.csv"
-    gstr2b_file = "returns_R2B_22AEZPA0778G1ZF_012022.json"
+    gstr2b_file = "returns_R2B_22AEZPA0778G1ZF_062022.json"
 
     spreadsheet_id = "1ZKTc8KJn5O7q7L8U2fUDXAYDHML0AmI7a5T8tWeKBr8"
-    sheet_name = "January 2022"
+    sheet_name = "June 2022"
     row_num = 8
 
-    """ gstr2acsv_to_google_sheets(
-        gstr2a_file, sheet_name, spreadsheet_id, row_num) """
-
-    gstr2bjson_to_google_sheets(
-        gstr2b_file, sheet_name, spreadsheet_id, row_num)
+    if gstr2b_file is not None:
+        gstr2bjson_to_google_sheets(
+            gstr2b_file, sheet_name, spreadsheet_id, row_num)
